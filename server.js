6b6618bf3b99cb86d46bba1f566ad51384c8183d
Alex Crimi <alexscrimi@gmail.com>
2018-05-05 21:33:51 -0700

    Builds out socket proxy

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const server = require('http').createServer();
+const socket = require('./lib/socket');
 const express = require('express');
 const app = express();
 
@@ -6,12 +8,25 @@ const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.set('view engine', 'pug');
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(port, ip, () => {
+app.post('/wake', (req, res) => {
+  const socketUrl = req.body.socketUrl;
+  socket.listen(socketUrl);
+  res.json({
+    proxy: socket.socketUrl
+  });
+});
+
+server.on('request', app);
+socket.use(server);
+
+server.listen(port, ip, () => {
   console.log('Server listening at:', ip + ':' + port);
 });
 
