68b8db393ab9573ef6c083786ba7985d23652240
Alex Crimi <alexscrimi@gmail.com>
2018-05-07 00:58:24 -0700

    Support env variables in config

--- config.js
+++
@@ -1,9 +0,0 @@
-function loadConfig() {
-  if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
-    return require('./config.production.json');
-  }
-
-  return require('./config.development.json');
-}
-
-module.exports = loadConfig();
